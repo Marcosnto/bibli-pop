c35256df09ae16cbdb3fd8940039eeca79ff740c
marcosnto <marcos_nto@hotmail.com>
2020-07-08 11:24:24 -0300

    Updates

---
+++ precache-manifest.7e4a72a9d682ca7d3345255b88d0a659.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7fdd8b7aef75bae3aa6",
+    "url": "/bibli-pop/static/css/main.26635d93.chunk.css"
+  },
+  {
+    "revision": "c7fdd8b7aef75bae3aa6",
+    "url": "/bibli-pop/static/js/main.c8b442a1.chunk.js"
+  },
+  {
+    "revision": "a35bb80d7aad143e695b",
+    "url": "/bibli-pop/static/js/runtime~main.f1b529fb.js"
+  },
+  {
+    "revision": "02079f982924cd123bb4",
+    "url": "/bibli-pop/static/css/2.d97afcd0.chunk.css"
+  },
+  {
+    "revision": "02079f982924cd123bb4",
+    "url": "/bibli-pop/static/js/2.7f8a8026.chunk.js"
+  },
+  {
+    "revision": "045a61e690d37313161b90c2bae814b1",
+    "url": "/bibli-pop/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/bibli-pop/precache-manifest.7e4a72a9d682ca7d3345255b88d0a659.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/bibli-pop/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
