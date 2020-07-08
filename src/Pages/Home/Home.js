9ac2631f2b88dc6369fdf015139b2a4d8ad88cc4
marcosnto <marcos_nto@hotmail.com>
2020-07-08 10:14:00 -0300

    Alteração para o gitpages

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,23 +1,21 @@
-import React, { Component, Fragment } from 'react';
-import 'materialize-css/dist/css/materialize.min.css';
-import './Home.scss'
-import Header from '../../Components/Header/Header'
-import Tabela from '../../Components/Table/Table'
-import PopUp from '../../utils/PopUp';
-import ApiService from '../../utils/ApiServices'
+import React, { Component, Fragment } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import "./Home.scss";
+import Header from "../../Components/Header/Header";
+import Tabela from "../../Components/Table/Table";
+import PopUp from "../../utils/PopUp";
+import ApiService from "../../utils/ApiServices";
 
 class Home extends Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       autors: [],
-    }
+    };
   }
 
   handleRemoveItem = id => {
-    
     const { autors } = this.state;
 
     const autoresAtualizados = autors.filter(autor => {
@@ -26,33 +24,50 @@ class Home extends Component {
 
     ApiService.RemoveAutor(id)
       .then(res => {
-        if (res.message === 'deleted') {
+        if (res.message === "deleted") {
           this.setState({ autors: [...autoresAtualizados] });
-          PopUp.exibeMensagem("success", "Autor Removido com sucesso");
+          PopUp.exibeMensagem("success", "Autor Removido com sucesso!");
         }
       })
-      .catch(err => PopUp.exibeMensagem("error", "Erro na comunicação com a API ao remover o usuario"));
-  }
+      .catch(err =>
+        PopUp.exibeMensagem(
+          "error",
+          "Erro na comunicação com a API ao remover o usuario"
+        )
+      );
+  };
 
   componentDidMount() {
     ApiService.ListaAutores()
       .then(res => {
-        if (res.message === 'success') {
-          this.setState({ autors: [...this.state.autors, ...res.data] })
+        if (res.message === "success") {
+          this.setState({ autors: [...this.state.autors, ...res.data] });
         }
       })
-      .catch(err => PopUp.exibeMensagem("error", "Erro na comunicação com a API ao Listar os usuarios"));
+      .catch(err =>
+        PopUp.exibeMensagem(
+          "error",
+          "Erro na comunicação com a API ao Listar os usuarios"
+        )
+      );
   }
 
   render() {
-    const campos = [{ titulo: 'Autores', dado: 'nome'}, { titulo: 'Livro', dado: 'livro'}, 
-    { titulo: 'Preços', dado: 'preco'}]
+    const campos = [
+      { titulo: "Autores", dado: "nome" },
+      { titulo: "Livro", dado: "livro" },
+      { titulo: "Preços", dado: "preco" },
+    ];
     return (
       <Fragment>
         <Header />
         <div className="container mb-10">
           <h2 className="center titulo">Acervo</h2>
-          <Tabela campos={campos} dados={this.state.autors} handleRemoveItem={this.handleRemoveItem}  />
+          <Tabela
+            campos={campos}
+            dados={this.state.autors}
+            handleRemoveItem={this.handleRemoveItem}
+          />
         </div>
       </Fragment>
     );
